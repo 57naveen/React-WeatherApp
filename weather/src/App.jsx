44b32c6452c6aa99818d3eeda706de7343a2eac7
57naveen <naveenkishore65@gmail.com>
2024-03-29 16:03:50 +0530

    FP

---
+++ weather/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+/* Images */
+
+import searchIcon from "./assets/searchIcon.png";
+import clearIcon from "./assets/clear.png";
+import cloudIcon from "./assets/cloudIcon.png";
+import drizzleIcon from "./assets/searchIcon.png";
+import searchIcon from "./assets/searchIcon.png";
+import searchIcon from "./assets/searchIcon.png";
+import searchIcon from "./assets/searchIcon.png";
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
