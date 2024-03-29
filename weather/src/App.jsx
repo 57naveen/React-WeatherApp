45a43d544f757592e52cdc844022c756d770c226
57naveen <naveenkishore65@gmail.com>
2024-03-29 18:13:41 +0530

    ede

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -4,21 +4,29 @@ import './App.css'
 
 /* Images */
 
-import searchIcon from "./assets/searchIcon.png";
-import clearIcon from "./assets/clear.png";
-import cloudIcon from "./assets/cloudIcon.png";
-import drizzleIcon from "./assets/searchIcon.png";
-import searchIcon from "./assets/searchIcon.png";
-import searchIcon from "./assets/searchIcon.png";
-import searchIcon from "./assets/searchIcon.png";
+import searchIcon from "./assets/search.png";
+// import clearIcon from "./assets/clear.png";
+// import cloudIcon from "./assets/cloud.png";
+// import drizzleIcon from "./assets/search.png";
+// import rainIcon from "./assets/rain.png";
+// import windIcon from "./assets/wind.png";
+// import snowIcon from "./assets/snow.png";
+// import humidityIcon from "./assets/humidity.png";
 
 function App() {
 
 
   return (
     <>
-      <div>
-        
+      <div className='container'>
+        <div className='input-container'>
+          <input type='text'
+          className='cityInput'
+          placeholder='Search City' />
+           <div className='search-icon'>
+          <img src={searchIcon} alt='Search'/>
+        </div>
+        </div>
       </div>
     </>
   )
